7e74bcbc173bbca46c87a44b4eed87a980388b67
Sufian <58152478+Darkx97@users.noreply.github.com>
2020-05-21 14:47:55 +0300

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,10 @@
-$(document).ready(function () 
-{
-    
 
-});
+    function hoverin(x)
+    {
+        x.style.color = "rgb(99, 55, 221)";
+    }
+
+    function hoverout(x)
+    {
+        x.style.color = " rgb(126, 126, 126)";
+    }
