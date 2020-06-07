88c40cae1f229da97b37c3bb806ffbb5a1001e1a
Sufian <58152478+Darkx97@users.noreply.github.com>
2020-06-07 08:46:03 +0300

    Add files via upload

--- JS/Index.js
+++ JS/Index.js
@@ -5,23 +5,12 @@ const elemnts = document.querySelectorAll("*");
 function navSlide()
 {
     const burger =document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+    const nav = document.querySelector('.nav-list');
     //toggle nav
         burger.addEventListener('click',()=>{
         nav.classList.toggle('nav-active');
 
         //animate links
-        navLinks.forEach((link, index) =>{
-            if(link.style.animation)
-            {
-                link.style.animation =''
-            }
-            else
-            {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/ 7 + 0.35}s` ;
-            }
-        }) 
 
         //burger animation
         burger.classList.toggle('toggle');
